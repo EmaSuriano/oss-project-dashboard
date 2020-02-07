0b4fe780c316f9434080cd7c441e48a1ec2bb4bc
EmaSuriano <emanuel.suriano@gmail.com>
2020-02-07 11:10:42 +0100

    move styles to custom styles to asssets

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import 'assets/vendor/nucleo/css/nucleo.css';
 import 'assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
 import 'assets/scss/argon-dashboard-react.scss';
-import './custom.css';
+import 'assets/custom.css';
 
 import AdminLayout from 'layouts/Admin.jsx';
 import AuthLayout from 'layouts/Auth.jsx';
