933000419d42794ebae52e12ff65c2e575e36f3e
EmaSuriano <emanuel.suriano@gmail.com>
2020-02-11 17:44:14 +0100

    add logo

--- src/index.js
+++ src/index.js
@@ -9,23 +9,37 @@ import 'assets/custom.css';
 
 import AdminLayout from 'layouts/Admin.jsx';
 import AuthLayout from 'layouts/Auth.jsx';
-import AuthProvider from './components/AuthProvider';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 // add logic to always redirect to /auth in case I don't have the token inside hte state of hte component.
 // maybe the authProvider it's not needed
 // Having all the state here will simplify things quite a lot
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <AuthProvider>
-        <Route path="/admin" render={props => <AdminLayout {...props} />} />
-        <Route path="/auth" render={props => <AuthLayout {...props} />} />
-        <Route exact path="/">
-          <Redirect to="/admin/index" />
-        </Route>
-      </AuthProvider>
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation =>
+    operation.setContext(() => ({
+      headers: {
+        Authorization: `bearer ${process.env.REACT_APP_GH_TOKEN}`,
+      },
+    })),
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <ApolloProvider client={client}>
+          <Route path="/admin" render={props => <AdminLayout {...props} />} />
+          <Route path="/auth" render={props => <AuthLayout {...props} />} />
+          <Route exact path="/">
+            <Redirect to="/admin/index" />
+          </Route>
+        </ApolloProvider>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
